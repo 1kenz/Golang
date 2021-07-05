5e4fe28e282543c062a0bb76630fee81543fca02
xkendx <63884610+xkendx@users.noreply.github.com>
2021-07-05 21:13:59 +0300

    feat: variables

--- learning/hello.go
+++ learning/hello.go
@@ -4,4 +4,17 @@ import "fmt"
 
 func main(){
 	fmt.Println("Hello, world!!!")
+
+	// var x int = 5
+	// var y int = 7
+	// var sum int = x + y
+
+	x := 5
+	y := 7
+	sum := x + y
+
+	fmt.Println(sum)
+
+
+
 }
