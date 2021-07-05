16e90e928f7c8a41a292fccacd22a1a8489c05a8
xkendx <63884610+xkendx@users.noreply.github.com>
2021-07-06 02:32:57 +0300

    feat: variables

---
+++ learning/golang-btk/01-Variables/main.go
@@ -0,0 +1,33 @@
+package main
+
+func main(){
+	// fmt.Println("Hello from go!")
+
+	//! Variables
+	// var word string = "Hello from go!"
+	// fmt.Println(word)
+	// fmt.Println(word)
+	// fmt.Println(word)
+	// fmt.Println(word)
+	// fmt.Println(word)
+
+	//? Integer
+	// var tax int = 15
+	// fmt.Println(tax)
+	// fmt.Println(100 + 100 * tax / 100)
+
+	//? Float
+	// var tax2 float32 = 0.1
+	// fmt.Println(tax2)
+	// fmt.Println(100 + 100 * tax2)
+
+	// var tax3 int = 111
+ 	// tax4 := "Ken Den"
+	
+	// fmt.Printf("Data type : %T", tax3)
+	// fmt.Printf("Data type : %T", tax4)
+
+	//! Boolean
+	
+
+}
