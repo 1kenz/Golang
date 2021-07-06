d8843b49037bd0c894b98336dcb5b8578392df46
xkendx <63884610+xkendx@users.noreply.github.com>
2021-07-07 01:23:10 +0300

    feat: define for loop

---
+++ learning/golang-btk/loops/forloopdemo1.go
@@ -0,0 +1,22 @@
+package loops
+
+func Demo1() {
+	var word string = "Hello from go!"
+
+	//! Define For Loops -- First Method
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println(word)
+	// }
+
+	//! Define For Loops -- Second Method
+	// i := 0
+	// for i < 5 {
+	// 	fmt.Println(word)
+	// 	i++
+	// }
+
+	// fmt.Println(word)
+	// fmt.Println(word)
+	// fmt.Println(word)
+	// fmt.Println(word)
+}
