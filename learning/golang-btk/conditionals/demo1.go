3f9b33d249c3373cd876790e64dbe6e7322a8dfc
xkendx <63884610+xkendx@users.noreply.github.com>
2021-07-06 04:03:35 +0300

    feat: if condition

--- learning/golang-btk/conditionals/demo1.go
+++ learning/golang-btk/conditionals/demo1.go
@@ -1,12 +1,15 @@
 package conditionals
 
-import "fmt"
-
 func Demo1() {
-	var account float32 = 1000
-	var draw float32 = 900
+	// var account float32 = 1000
+	// var draw float32 = 900
 
-	if account > draw {
-		fmt.Println("No sufficient money in your account")
-	}
+	// if account < draw {
+	// 	fmt.Println("No sufficient money in your account")
+	// } else if account >= draw {
+	// 	fmt.Println("Preparing your money...")
+	// 	account = account - draw
+	// }
+	// fmt.Println("Your money is ready. Now your balance : " + fmt.Sprintf("%v", account))
+	// fmt.Printf("Your money is ready. Now your balance : %v", account)
 }
