79b0f5fcbceedff572c7fc208b41a997c2ff7302
xkendx <63884610+xkendx@users.noreply.github.com>
2021-07-08 02:54:03 +0300

    feat: add 0-100 limit

--- learning/golang-btk/loops/workshop.go
+++ learning/golang-btk/loops/workshop.go
@@ -12,13 +12,21 @@ func Workshop() {
 	prediction := 0
 
 	predictionCounter := 0
+	
 
-	fmt.Println("Start prediction!")
+	fmt.Println("Start prediction! (0-100)")
 	fmt.Scanln(&prediction)
+	
+	if prediction > 100 || prediction < 0 {
+		fmt.Println("Wrong number! Please insert 0-100 number.")
+		fmt.Println("Start prediction! (0-100)")
+		fmt.Scanln(&prediction)
+	}
+		
 	predictionCounter += 1
 
-
 	for number != prediction {
+		
 
 		if number < prediction {
 			fmt.Println("Lower")
