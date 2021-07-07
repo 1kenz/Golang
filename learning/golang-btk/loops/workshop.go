3fa76173fefda25155a7b9f4271feb1e95cb65cf
xkendx <63884610+xkendx@users.noreply.github.com>
2021-07-07 03:26:05 +0300

    feat: prediction number

--- learning/golang-btk/loops/workshop.go
+++ learning/golang-btk/loops/workshop.go
@@ -9,19 +9,24 @@ func Workshop() {
 
 	number := 55
 	
-	var prediction int = 56
+	prediction := 0
+
+	fmt.Println("Start prediction!")
+	fmt.Scanln(&prediction)
 
 
-	if number < prediction {
-		fmt.Println("Lower")
-	} else if number > prediction {
-		fmt.Println("Higher")
-	} else {
-		fmt.Println("Thats correct!!!")
+	for number != prediction {
+
+		if number < prediction {
+			fmt.Println("Lower")
+			fmt.Scanln(&prediction)
+		} else if number > prediction {
+			fmt.Println("Higher")
+			fmt.Scanln(&prediction)
+		}
 	}
+	fmt.Println("Thats correct!!!")
 
-
-
-	// fmt.Println(prediction)
-	// fmt.Println(number)
+				// fmt.Println(prediction)
+				// fmt.Println(number)
 }
