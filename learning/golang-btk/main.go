7d28b077ba972ec51c36f23826f8a42122f44769
xkendx <63884610+xkendx@users.noreply.github.com>
2021-07-09 02:19:04 +0300

    feat: loops workshop

--- learning/golang-btk/main.go
+++ learning/golang-btk/main.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"golessons/loops"
-)
-
-
-
 func main() {
 	// variables.Demo1()
 	// fmt.Println()
@@ -13,9 +7,11 @@ func main() {
 	// conditionals.Demo1()
 	// conditionals.Demo2()
 	// conditionals.Workshop()
+
 	// loops.Demo1()
 	// loops.Demo2()
 	// loops.Workshop()
 	// loops.Workshop2()
-	loops.Workshop3()
+	// loops.Workshop3()
+
 }
