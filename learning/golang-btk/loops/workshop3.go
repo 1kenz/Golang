4e89f338247e326bb91273400c7e2b5fd90d3f8f
xkendx <63884610+xkendx@users.noreply.github.com>
2021-07-08 03:47:35 +0300

    feat: friend numbers refactor

--- learning/golang-btk/loops/workshop3.go
+++ learning/golang-btk/loops/workshop3.go
@@ -6,8 +6,8 @@ import "fmt"
 
 
 func Workshop3() {
-	number1 := 11  // 220
-	number2 := 44  // 284
+	number1 := 1184  // 11  // 220
+	number2 := 1210  // 44  // 284
 	sum1 := 0
 	sum2 := 0
 	
@@ -15,15 +15,17 @@ func Workshop3() {
 		if number1%i == 0 {
 			sum1 = sum1 + i
 		}
-
+	}
+	
+	for i := 1; i < number2; i++ {
 		if number2%i == 0 {
 			sum2 = sum2 + i
 		}
-	} 
-	
-		if sum1 == number2 && sum2 == number1 {
-			fmt.Println("This numbers are friend")
-		} else {
-			fmt.Println("These numbers are not friend")
-		}
+	}
+
+	if sum1 == number2 && sum2 == number1 {
+		fmt.Printf("%v and %v are friend numbers", number1, number2)
+	} else {
+		fmt.Printf("%v and %v are not friend numbers", number1, number2)
+	}
 }
